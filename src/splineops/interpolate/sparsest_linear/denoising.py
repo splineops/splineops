9126f6d4996bcaf322d84236e124c92456a04472
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-01-29 17:07:42 +0100

    Add sparsest linear module

---
+++ src/splineops/interpolate/sparsest_linear/denoising.py
@@ -0,0 +1,111 @@
+from typing import Tuple
+
+import numpy as np
+import scipy.sparse as sp
+
+
+def denoise_y(x: np.ndarray, y: np.ndarray, lamb: float, rho: float = 1.0, max_iter: int = int(1e4),
+                       relative_tol: float = 1e-7) -> np.ndarray:
+    """ 
+    Solve Problem (36) in [1]_ using ADMM [2]_.
+
+    Parameters
+    ----------
+    x : ndarray
+        Array of x-coordinates of data points
+    y : ndarray
+        Array of y-coordinates of data points
+    lamb : float
+        Regularization parameter lambda
+    rho : float
+        Internal ADMM parameter [2]_
+    max_iter : int
+        Maximum number of iterations for ADMM
+    relative_tol : float
+        Tolerance parameter for ADMM stopping criterion (:math:`\epsilon^\mathrm{abs}` in [2]_)
+
+    Returns
+    -------
+    y_lambda : ndarray
+        Array of denoised y-coordinates of data points (:math:`\mathbf{y}_\lambda` in [1]_)
+
+    References
+    ----------
+    .. [1] Debarre, T., Denoyelle, Q., Unser, M., and Fageot, J. "Sparsest Continuous Piecewise-Linear Spline
+           Representation of One-Dimensional Data." Journal of Computational and Applied Mathematics, 2022.
+
+    .. [2] Boyd, S., et al. “Distributed Optimization and Statistical Learning via the Alternating Direction
+           Method of Multipliers.” Foundations and Trends in Machine Learning, 2011.
+    """
+    if x.size != y.size:
+        raise Exception("x and y must be of the same size")
+    lamb_max, polynomial = _lambda_max(x, y)
+    if lamb >= lamb_max:
+        # If lamb is too high, the problem amounts to linear regression
+        y_denoised = polynomial[0] * np.ones_like(x) + polynomial[1] * x
+    elif lamb > 0:
+        # Otherwise, solve denoising problem using ADMM
+        # Define matrices
+        L = _regularization_matrix(x)  # Regularization matrix
+        Lt = L.transpose()
+        M = sp.identity(len(x)) + rho * (Lt @ L)  # Matrix to be inverted at every x-update step in ADMM
+        # ADMM initialization
+        xk, zk, yk = y, L @ y, np.zeros(L.shape[0])
+        # Run ADMM
+        for it in range(max_iter):
+            z_prev = zk  # Needed for stopping criterion
+            # ADMM updates (notations follow Boyd et al. 2011)
+            b = y + rho * Lt @ (zk - yk / rho)
+            xk = sp.linalg.spsolve(M, b)
+            Lxk = L @ xk  # Precomputation
+            zk = _prox_L1(Lxk + yk / rho, lamb / rho)
+            yk += rho * (Lxk - zk)
+
+            # ADMM stopping criterion (as suggested by Boyd et al. 2011)
+            primal_res = np.linalg.norm(Lxk - zk)
+            dual_res = rho * np.linalg.norm(Lt @ (zk - z_prev))
+            primal_eps = relative_tol * max(np.linalg.norm(Lxk), np.linalg.norm(zk))
+            dual_eps = relative_tol * np.linalg.norm(Lt @ yk)
+            if (primal_res <= primal_eps) and (dual_res <= dual_eps):
+                break
+        y_denoised = xk
+
+    else:
+        # If lamb = 0, no denoising
+        y_denoised = y
+    return y_denoised
+
+
+def _lambda_max(x: np.ndarray, y: np.ndarray) -> Tuple[float, np.ndarray]:
+    """ 
+    Compute maximum regularization parameter lambda above which the problem amounts to linear regression.
+     Returns maximum lambda and the linear regression solution. 
+     """
+    if x.size != y.size:
+        raise Exception("x and y must be of the same size")
+    m = len(x)
+
+    # Compute parameters of polynomial (solution of a 2x2 system)
+    det = m * np.sum(x ** 2) - np.sum(x) ** 2
+    polynomial = (1 / det) * np.array([[np.sum(x ** 2), -np.sum(x)], [-np.sum(x), m]]).dot(
+        np.array([np.sum(y), np.dot(x, y)]))
+
+    h = y - (polynomial[0] * np.ones_like(x) + polynomial[1] * x)
+    lamb_max = max(np.abs(x[1:-1] * np.cumsum(h)[:-2] - np.cumsum(h * x)[:-2]))
+    return lamb_max, polynomial
+
+
+def _regularization_matrix(x: np.ndarray) -> sp.diags:
+    """ 
+    Compute the L matrix defined in Equation (37) in Debarre et al. 2022.
+    """
+    M = len(x)
+    v = 1 / (x[1:] - x[:-1])
+    return sp.diags([v[:-1], -(v[:-1] + v[1:]), v[1:]], [0, 1, 2], shape=(M-2, M))
+
+
+def _prox_L1(x: np.ndarray, sigma: float):
+    """ 
+    Compute proximal operator of the L1 norm. 
+    """
+    return np.sign(x) * np.maximum(np.abs(x) - sigma, 0)
---
+++ src/splineops/interpolate/sparsest_linear/sparsification.py
@@ -0,0 +1,152 @@
+from typing import Tuple
+
+import numpy as np
+
+
+def sparsest_interpolant(x: np.ndarray, y: np.ndarray, sparsity_tol: float = 1e-5) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """ 
+    Algorithm 1 in [1]_ to compute the linear spline that connects given data points with the fewest knots.
+
+    The optimal spline can be evaluated using the `linear_spline()` function with the outputs of this function.
+
+    Parameters
+    ----------
+    x : ndarray
+        Array of x-coordinates of data points
+    y : ndarray
+        Array of y-coordinates of data points
+    sparsity_tol : float
+        Knots whose amplitude is smaller in absolute value than this tolerance parameter are discarded.
+
+    Returns
+    -------
+    knots : ndarray
+        Array of knots of the optimal spline
+    amplitudes : ndarray
+        Array of amplitudes of these knots
+    polynomial : ndarray
+        Size-2 array (b, a) parametrizing the linear component p(t) = at + b of the optimal spline
+
+    References
+    ----------
+    .. [1] Debarre, T., Denoyelle, Q., Unser, M., and Fageot, J. "Sparsest Continuous Piecewise-Linear Spline
+           Representation of One-Dimensional Data." Journal of Computational and Applied Mathematics, 2022.
+    """
+    if x.size != y.size:
+        raise Exception("x and y must be of the same size")
+
+    knots = x[1:-1]
+    amplitudes_cano, polynomial_cano = _connect_points(x, y)
+
+    # Identify phantom knots (amplitude = 0) which are outside of saturation zones
+    saturations = _saturation_zones(amplitudes_cano, sparsity_tol)
+    pruned_bool = np.logical_or(saturations != 0, np.abs(amplitudes_cano) > sparsity_tol)
+    # Remove these phantom knots
+    knots_pruned = knots[pruned_bool]
+    amplitudes_pruned = amplitudes_cano[pruned_bool]
+    saturations_pruned = saturations[pruned_bool]
+    #amplitudes_pruned = sparsify_amplitudes(amplitudes_pruned, sparsity_tol)  # Set knots below tolerance to exactly zero
+
+    # Sparsification of saturation zones
+    amplitudes_sparsest = np.array([])
+    knots_sparsest = np.array([])
+    i = 0
+    last_nz_idx = 0
+    num_saturations = 0  # Number of consecutive saturation intervals after knots_pruned[i]
+    while i < len(knots_pruned):
+        if saturations_pruned[i] != 0:
+            num_saturations = saturations_pruned[i]
+        for j in range(int(np.ceil(num_saturations / 2))):
+            amplitudes_sparsest = np.append(amplitudes_sparsest, amplitudes_pruned[i+2*j] + amplitudes_pruned[i+2*j+1])
+            knots_sparsest = np.append(knots_sparsest, (amplitudes_pruned[i+2*j] * knots_pruned[i+2*j] +
+                                   amplitudes_pruned[i+2*j+1] * knots_pruned[i+2*j+1]) / amplitudes_sparsest[-1])
+        if (num_saturations % 2) == 0:
+            # Keep last existing knot if even number of saturations (including 0)
+            amplitudes_sparsest = np.append(amplitudes_sparsest, amplitudes_pruned[i+num_saturations])
+            knots_sparsest = np.append(knots_sparsest, knots_pruned[i+num_saturations])
+
+        i += num_saturations + 1
+        last_nz_idx += 1
+        num_saturations = 0
+
+    return knots_sparsest, amplitudes_sparsest, polynomial_cano
+
+
+def linear_spline(t: np.ndarray, knots: np.ndarray, amplitudes: np.ndarray, polynomial: np.ndarray) -> np.ndarray:
+    """
+    Evaluate a parametrized linear spline at location(s) t.
+
+    The mathematical expression of the spline is :math:`s(t) = at + b + \\sum_{k=0}^{K} a_k (t - \\tau_k)_+`, where
+    a and b are the parameters of the linear component and a_k and \\tau_k are the amplitudes and the locations of the
+    knots, respectively.
+
+    Parameters
+    ----------
+    t : float or ndarray
+        Evaluation point(s) of the spline
+    knots : ndarray
+        Array of knots of the linear spline
+    amplitudes : ndarray
+        Array of amplitudes of these knots
+    polynomial : ndarray
+        Size-2 array (b, a) parametrizing the linear component p(t) = at + b of the spline
+
+    Returns
+    -------
+    values : float or ndarray
+        Value(s) of the spline at the evaluation point(s)
+    """
+    values = polynomial[0] + polynomial[1] * t
+    for i in range(len(knots)):
+        values = values + amplitudes[i] * (t - knots[i]) * ((t - knots[i]) > 0)
+    return values
+
+
+def _sparsify_amplitudes(amplitudes: np.ndarray, sparsity_tol: float = 1e-5) -> np.ndarray:
+    """ 
+    Adjust amplitudes by setting those below the tolerance to zero while conserving the linear spline exactly
+    outside areas with phantom knots. 
+    """
+    zero_indices = np.nonzero(np.abs(amplitudes) <= sparsity_tol)
+    amplitudes_sparsified = amplitudes
+    amplitudes_sparsified[zero_indices] = 0  # Set knots below tolerance to zero
+    i = 0
+    while i < len(zero_indices):
+        # Compensate close to zero amplitudes on previous knot
+        amplitudes_sparsified[zero_indices[i]-1] += amplitudes_sparsified[zero_indices[i]]
+        if i == len(zero_indices) - 1:
+            break
+        j = 0
+        # If consecutive phantom knots, compensate all of them on the closest previous true knot
+        while i + j + 1 < len(zero_indices) and zero_indices[i+j+1] == zero_indices[i+j] + 1:
+            amplitudes_sparsified[zero_indices[i]-1] += amplitudes_sparsified[zero_indices[i+j+1]]
+            j += 1
+        i += j + 1
+    return amplitudes_sparsified
+
+
+def _saturation_zones(amplitudes: np.ndarray, sparsity_tol: float = 1e-5) -> np.ndarray:
+    """ 
+    Return array specifying number of consecutive saturation zones (limit points included). 
+    """
+    saturations = np.zeros_like(amplitudes)
+    nz_idx = np.nonzero(np.abs(amplitudes) > sparsity_tol)[0]
+    if len(nz_idx) > 0:
+        sat_idx_start = nz_idx[0]
+        for i in range(len(nz_idx)-1):
+            if np.sign(amplitudes[nz_idx[i]]) != np.sign(amplitudes[nz_idx[i+1]]):
+                saturations[sat_idx_start:nz_idx[i]+1] = nz_idx[i] - sat_idx_start
+                sat_idx_start = nz_idx[i+1]
+        saturations[sat_idx_start:nz_idx[-1]+1] = nz_idx[-1] - sat_idx_start
+
+    return saturations.astype(int)
+
+
+def _connect_points(x: np.ndarray, y: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    """ 
+    Return parametrization of the canonical linear-spline solution that connects the data points (x[i], y[i]). 
+    """
+    slopes = (y[1:] - y[:-1]) / (x[1:] - x[:-1])
+    polynomial = np.array([y[0] - slopes[0] * x[0], slopes[0]])
+    amplitudes = slopes[1:] - slopes[:-1]
+    return amplitudes, polynomial
