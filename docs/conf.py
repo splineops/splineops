c6a2db9059fb273526e5d1a5418d5cdcf51066cc
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-05-26 13:47:18 +0200

    Update pyproject license field

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("sphinxext"))
 # Project information
 project = 'splineops'
 copyright = f'{datetime.now().year}, SplineOps authors'
-release = '0.3.7'
+release = '0.3.8'
 
 # General configuration
 extensions = [
