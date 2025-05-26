04dfd1dccdf8500365ec2e59ae107d726510505c
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-05-26 14:24:30 +0200

    Update license file

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("sphinxext"))
 # Project information
 project = 'splineops'
 copyright = f'{datetime.now().year}, SplineOps authors'
-release = '0.4.2'
+release = '0.4.3'
 
 # General configuration
 extensions = [
