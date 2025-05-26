2c140a79815e11efb9dc268ccfa45d66f7f3adad
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-05-26 14:14:37 +0200

    Update BSD-3-Clause

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("sphinxext"))
 # Project information
 project = 'splineops'
 copyright = f'{datetime.now().year}, SplineOps authors'
-release = '0.3.9'
+release = '0.4.0'
 
 # General configuration
 extensions = [
