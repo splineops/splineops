2948b6fef1e4608716d1c4733670112135f47428
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-10-09 10:39:33 +0200

    Fix zn in least-squares utils

--- src/splineops/resize/utils.py
+++ src/splineops/resize/utils.py
@@ -392,7 +392,7 @@ def get_initial_causal_coefficient(
 
     n = np.arange(1, horizon - 1)
     z1_array = z ** n
-    zn_array = (zn / z ** n) * z ** n  # simplifies to zn since (zn / z**n) * z**n = zn
+    zn_array = zn / (z ** n)
     sum_ += np.sum((z1_array + zn_array) * c[1:horizon-1])
 
     return sum_ / (1.0 - z ** (2 * len(c) - 2))
