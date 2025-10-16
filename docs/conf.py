d95304088691d1900f30cf779aff08b9f93b6c46
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-10-16 11:14:27 +0200

    Update version number

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ sys.path.insert(0, os.path.abspath("sphinxext"))
 # Project information
 project = 'splineops'
 copyright = f'{datetime.now().year}, SplineOps authors'
-release = '0.5.0'
+release = '0.5.1'
 
 # General configuration
 extensions = [
