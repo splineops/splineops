ea99fc99ca9f1e7dc35964e9a23cf6fe7ca63a0c
Pablo Garcia-Amorena <pablogamorena@gmail.com>
2025-11-21 19:30:57 +0100

    Update parameters

--- scripts/script_resize_2d_plot_pytorch_opencv.py
+++ scripts/script_resize_2d_plot_pytorch_opencv.py
@@ -73,7 +73,7 @@ from splineops.resize import resize as spl_resize
 DTYPE = np.float32
 DTYPE_NAME = np.dtype(DTYPE).name
 
-MARKER_SIZE = 5
+MARKER_SIZE = 3
 
 # -------------------------- UI / I/O helpers --------------------------
 
